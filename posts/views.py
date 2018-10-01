9f025f8bee46506ad8cae54cfdfb143f4b4af4bc
corti7 <coa_777@hotmail.com>
2018-09-30 22:10:44 -0500

    Templates

--- posts/views.py
+++ posts/views.py
@@ -1,39 +1,45 @@
 #Django
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 #Utilities
-from datetime import datetime
+from datetime import datetime, date
 
 # Create your views here.
 
+# Import your app
+
+
 posts = [
-    {
-        'name': 'Mont Blac',
-        'user': 'Yessica Cortez',
+     {
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'http://picsum.photos/200/200/?=1037'
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name': 'Dark Line',
-        'user': 'Julio Contreras',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'http://picsum.photos/200/200/?=1045'
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'name': 'Green Gold',
-        'user': 'Pedro Martinez',
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture': 'http://picsum.photos/200/200/?=1058'
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
 def list_posts(request):
-    content = []
-    for post in posts:
-        content.append("""
-            <p><strong>{name}</strong></p>
-            <p><small>{user} - <i>{timestamp}</i></small></p>
-            <figure><img src="{picture}"/></figure>
-        """.format(**post))
-    return HttpResponse('<br>'.join(content))
+    return render(request,'feed.html', {'posts': posts})
+
