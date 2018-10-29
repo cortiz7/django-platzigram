cd289a3d8f7fa11f0db40102700dc11cd8e20f92
corti7 <coa_777@hotmail.com>
2018-10-28 22:43:31 -0500

    feat: Sigunp user

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('posts', posts_views.list_posts, name='posts'),
     path('users/login/', users_views.login_view, name='login'),
     path('users/logout/', users_views.logout_view, name='logout'),
+    path('users/signup/', users_views.signup_view, name='signup'),
 
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
+#Django
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-# Create your views here.
+#Models
+from django.contrib.auth.models import User
+from users.models import Profile
 
+#Exception
+from django.db.utils import IntegrityError
+
+# Create your views
 def login_view(request):
     #import pdb; pdb.set_trace()
     if request.method == 'POST':
@@ -21,3 +28,26 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirm = request.POST['password_confirm']
+        if password != password_confirm:
+            return render(request, 'users/signup.html', {'error' : 'Password confirmation does not match'})
+        
+        try:
+            user = User.objects.create_user(username=username, password=password)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error' : 'Username is already in user'})
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
