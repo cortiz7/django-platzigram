d9820d89d6bcf59d670fd3eda20103281b5d675e
corti7 <coa_777@hotmail.com>
2018-10-28 23:27:54 -0500

    feat: Middleware validation picture and biograpy of profile

---
+++ platzigram/middleware.py
@@ -0,0 +1,33 @@
+"""Platzigram middleware catalog  """
+
+#Django 
+from django.shortcuts import redirect
+from django.urls import reverse
+
+#Models
+from users.models import Profile
+
+class ProfileCompletionMiddleware:
+    """Profile completion middleware  
+    
+    Ensure every user that is interacting with thw platform
+    have their profile picture and biography
+    """
+
+    def __init__(self, get_response):
+        """ Middleware inizialition """
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        """ Code to be executed for each request before the view is called """
+        if not request.user.is_anonymous:
+            #profile = request.user.profile
+            profile = Profile(user=request.user)
+            if not profile.picture or not profile.biography:
+                if request.path not in [reverse('update_profile'), reverse('logout')]:
+                    return redirect('update_profile')
+
+        response = self.get_response(request)
+        return response 
+            
+
--- users/views.py
+++ users/views.py
@@ -51,3 +51,7 @@ def signup_view(request):
         return redirect('login')
 
     return render(request, 'users/signup.html')
+
+def update_profile(request):
+    """Update Profile """
+    return render(request, 'users/update_profile.html')
